e9daa2bc70a9387a5e01622fe6fd15db57edce95
sangeeth911 <sangeeth999123@gmail.com>
2024-12-10 18:12:03 +0530

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Button, ChakraProvider } from '@chakra-ui/react';
-// import theme from './theme/theme';
-
+import { Button, ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
+import theme from "./theme/theme";
+import { ThemeToggle } from "./components/ThemeToggle";
 function App() {
   return (
-    <ChakraProvider >
-      <Button>Click me</Button>
-
+    <ChakraProvider theme={theme}>
+      <Grid templateAreas={`"nav nav" "aside main"`}>
+        <GridItem area="nav" bg="coral">
+          {" "}
+          Nav
+        </GridItem>
+        <GridItem area="aside" bg="blue.500">
+          {" "}
+          Aside
+        </GridItem>
+        <GridItem area="main" bg="green.500">
+          {" "}
+          Main
+        </GridItem>
+      </Grid>
     </ChakraProvider>
   );
 }
