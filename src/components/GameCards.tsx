293125868e01be4868b871b89e33b62dfe867e7a
sangeeth911 <sangeeth999123@gmail.com>
2024-12-10 21:55:06 +0530

    Refactor GameCards component to use custom hook for fetching games

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -1,33 +1,15 @@
 // import React from 'react'
 
-import apiClient from "../services/api-client";
-import { useEffect, useState } from "react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameCards = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient.get<FetchGameResponse>('/games')
-      .then(res => setGames(res.data.results))
-      .catch(error => setError(error.message))
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
-    {error && <p>{error}</p>}
+      {error && <p>{error}</p>}
       <ul>
-        {games.map(game => (
+        {games.map((game) => (
           <li key={game.id}>{game.name}</li>
         ))}
       </ul>
