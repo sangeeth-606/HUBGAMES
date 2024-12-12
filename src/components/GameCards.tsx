d3c5794795597e763328943d9b6609710954f026
sangeeth911 <sangeeth999123@gmail.com>
2024-12-12 23:42:07 +0530

    loading screen

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -3,14 +3,18 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameBox from "./GameBox";
+import GameSkeletos from "./GameSkeletos";
 
 const GameCards = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
 
   return (
     <>
       {error && <p>{error}</p>}
       <SimpleGrid columns={ { sm:1 , md:2 , lg:3 , xl:4 } } padding={10} spacing={10} overflow="hidden" >
+        {isLoading && games.map((game) => (
+          <GameSkeletos key={game.id}/>
+        ))} 
         {games.map((game) => (
           <GameBox key={game.id} game={game}/>
         ))} 
