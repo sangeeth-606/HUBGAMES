7cb7d8056686a972417a81f2d54c6fb8cb5f9b6e
sangeeth911 <sangeeth999123@gmail.com>
2024-12-12 16:02:04 +0530

    displaying platform icons

--- src/components/GameBox.tsx
+++ src/components/GameBox.tsx
@@ -1,6 +1,7 @@
 // import React from 'react'
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./Platformiconlist";
 
 interface Props {
     game: Game
@@ -10,9 +11,10 @@ interface Props {
 const GameBox = ( {game}:Props ) => {
   return (
     <Card>
-        <Image src={game.background_image} ></Image>
+        <Image src={game.background_image} borderRadius='10px 10px 0 0'></Image>
         <CardBody>
             <Heading fontSize="2xl" > {game.name} </Heading>
+            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
         </CardBody>
     </Card>
   )
